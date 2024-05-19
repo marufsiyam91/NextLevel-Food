1f38c242c2f8689de9094abf404aa56e52d1c1ff
marufsiyam91 <marufsiyam9123@gmail.com>
2024-05-19 22:46:39 +0600

    meals loading and error handled

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,32 @@
-export default function SingleMeal ({params}) {
-    return (
-        <div>
-            {params.mealSlug}
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default function SingleMeal({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
-    )
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions, }}
+        ></p>
+      </main>
+    </>
+  );
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,29 +1,34 @@
-import Link from 'next/link'
-import styles from './page.module.css'
-import MealsGrid from '@/Components/meals/meals-grid'
-import getMeals from '@/lib/meals'
+import Link from "next/link";
+import { Suspense } from "react";
+import styles from "./page.module.css";
+import MealsGrid from "@/Components/meals/meals-grid";
+import getMeals from "@/lib/meals";
 
-export default async function MealPage () {
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
-    const meals = await getMeals()
-
-
-    return (
-        <>
-            <header className={styles.header}>
-                <h1>
-                    Delicious meals, created <span className={styles.highlight}>by you</span>
-                </h1>
-                <p>Choose your favourite recipe and cook it yourself. It is easy and fun!</p>
-                <p className={styles.cta}>
-                    <Link href={'/meals/share'}>
-                        Share your favourite racipe
-                    </Link>
-                </p>
-            </header>
-            <main className={styles.main}>
-                <MealsGrid meals={meals}/>
-            </main>
-        </>
-    )
+export default async function MealPage() {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href={"/meals/share"}>Share your favourite racipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <Suspense fallback={<p className={styles.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
